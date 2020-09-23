92c560e57c898480c8152b5bb25a451c0482110a
Aran1992 <2572261667@qq.com>
2020-09-23 23:49:18 +0800

    second commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,13 @@ public class Player : MonoBehaviour
     public float vecocity;
     public float jumpForce;
     Rigidbody2D body;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,12 @@ public class Player : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             body.AddForce(new Vector2(0, jumpForce));
+            animator.SetBool("run2jump", true);
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        animator.SetBool("run2jump", false);
     }
 }
